66ef9b4bb6a11d648eb9aa3390f0a77a4b382155
Fabrizzio <asanchezc@uni.pe>
2024-11-10 12:03:35 -0500

    Clases GestionUsuario y GestionTarea: Se desarrollo las clases para la gestion de usuarios y tareas (agregar y listar)

--- src/servicios/GestionTarea.java
+++ src/servicios/GestionTarea.java
@@ -1,4 +1,59 @@
 package servicios;
 
+import clases.Tarea;
+import clases.Usuario;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GestionTarea {
+    private List<Tarea> tareas = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void agregarTarea(GestionUsuario gestionUsuario) {
+        System.out.print("Ingrese el título de la tarea: ");
+        String titulo = scanner.nextLine();
+        System.out.print("Ingrese la descripción: ");
+        String descripcion = scanner.nextLine();
+        System.out.print("Ingrese la fecha de entrega (YYYY-MM-DD): ");
+        LocalDate fechaEntrega = LocalDate.parse(scanner.nextLine());
+        System.out.print("Ingrese la prioridad (alta, media, baja): ");
+        String prioridad = scanner.nextLine();
+        System.out.print("Ingrese el estado (pendiente, en progreso, completada): ");
+        String estado = scanner.nextLine();
+
+        gestionUsuario.mostrarUsuarios();
+        System.out.print("Seleccione el nombre del usuario asignado: ");
+        String nombreUsuario = scanner.nextLine();
+        Usuario usuarioAsignado = gestionUsuario.buscarUsuarioPorNombre(nombreUsuario);
+
+        if (usuarioAsignado != null) {
+            tareas.add(new Tarea(titulo, descripcion, fechaEntrega, prioridad, estado, usuarioAsignado));
+            System.out.println("Tarea agregada exitosamente.\n");
+        } else {
+            System.out.println("Usuario no encontrado.\n");
+        }
+    }
+
+    public void mostrarTareas() {
+        System.out.printf("""
+                \nLista de Tareas:
+                ===================================================================================================
+                %-20.20s%4s%-35.35s%4s%-12.12s%4s%-20.20s
+                ===================================================================================================
+                ""","Título","","Descripción","","Estado","","Usuario");
+        for (Tarea tarea : tareas) {
+            System.out.printf("""
+                    %-20.20s%4s%-35.35s%4s%-12.12s%4s%-20.20s
+                    """,tarea.getTituloTarea(),"",tarea.getDescripcionTarea(),"",tarea.getEstadoTarea(),"",
+                    tarea.getUsuarioAsignadoTarea().getNombreUsuario());
+        }
+        System.out.println();
+    }
+
+    public List<Tarea> listarTareas() {
+        return tareas;
+    }
 }
